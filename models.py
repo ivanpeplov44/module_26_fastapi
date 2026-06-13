667ce5b363892ea7957007470c583e9dafae72c9
ivanpeplov44 <ivan.peplov44@gmail.com>
2026-06-13 21:47:59 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+class Book(Base):
+    __tablename__ = 'Book'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    call_count = Column(Integer, default=0)
